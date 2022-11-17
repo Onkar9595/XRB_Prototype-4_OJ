c71882b4c7e54713d5dc3409b9357af7ca884e29
Onkar9595 <111443758+Onkar9595@users.noreply.github.com>
2022-11-16 19:12:45 -0500

    Wip

--- Assets/Scripts/MotionLight.cs
+++ Assets/Scripts/MotionLight.cs
@@ -3,20 +3,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MotionLight : MonoBehaviour
 {
     [SerializeField] private Light _motionLight;
+  
+
+    public bool isFlicekring = false;
+
+    public float timeDelay =2f;
+
+    [SerializeField] private Light pointLightForEmmisive;
 // Start is called before the first frame update
     void Start()
     {
         _motionLight.enabled = false;
+        isFlicekring = false;
+        pointLightForEmmisive.enabled = false;
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
         _motionLight.enabled = true;
+        pointLightForEmmisive.enabled = true;
+        if (isFlicekring == false)
+        {
+            StartCoroutine(FlickeringLight());
+        }
+
+        IEnumerator FlickeringLight()
+        {
+            isFlicekring = true;
+            _motionLight.GetComponent<Light>().enabled = false;
+            yield return new WaitForSeconds(timeDelay);
+            _motionLight.GetComponent<Light>().enabled = true;
+            isFlicekring = false;
+        }
     }
 
+    private void OnTriggerExit(Collider other)
+    { 
+        _motionLight.enabled = false;
+        pointLightForEmmisive.enabled = false;
+    }
     // Update is called once per frame
     void Update()
     {
